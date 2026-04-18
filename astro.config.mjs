fd18c92cd322cb36c063f9d435e3dc355e5eccb8
Edinson Rivera <edirju@gmail.com>
2026-04-18 12:08:30 -0500

    chrore: manual migration to vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,34 +8,15 @@ import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 
 import node from '@astrojs/node';
-
-import netlify from '@astrojs/netlify';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   vite: {
     plugins: [tailwindcss()],
-    build: {
-      rollupOptions: {
-        external: (id) => {
-          if (id.includes("astro/entrypoints") || id.includes("astro/dist")) {
-            return false;
-          }
-          return false;
-        } 
-      },
-    },
-    ssr: {
-      noExternal: ["lucide-react", "nanostores", "@nanostores/react"],
-    },
   },
 
   integrations: [react(), svelte()],
-  adapter: netlify(),
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
+  adapter: vercel(),
 });
